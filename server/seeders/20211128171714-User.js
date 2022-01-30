a655d6d32d04cda39b56e78a8e1ee9478f12eed9
shaghnanii <shakilsak05@gmail.com>
2022-01-31 00:07:53 +0500

    updated seeder for users

--- server/seeders/20211128171714-User.js
+++ server/seeders/20211128171714-User.js
@@ -14,28 +14,28 @@ module.exports = {
         image: "https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-1520x800.jpeg",
         role_id: 1,
       },
-      {
-        email: "aunsia.khan@szabist-isb.pk",
-        password:
-            "$2a$08$EwfXXUfdO0fdItc4/kpuGOWkb/N0pHqHajNOkv0eiclOevQXvM6v.",
-        is_active: 1,
-        reg_id: 1111111,
-        password_status: 0,
-        reset_code: null,
-        image: "https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-1520x800.jpeg",
-        role_id: 3,
-      },
-      {
-        email: "tazeen.athar@szabist-isb.pk",
-        password:
-            "$2a$08$EwfXXUfdO0fdItc4/kpuGOWkb/N0pHqHajNOkv0eiclOevQXvM6v.",
-        is_active: 1,
-        reg_id: 2222222,
-        password_status: 0,
-        reset_code: null,
-        image: "https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-1520x800.jpeg",
-        role_id: 2,
-      },
+      // {
+      //   email: "aunsia.khan@szabist-isb.pk",
+      //   password:
+      //       "$2a$08$EwfXXUfdO0fdItc4/kpuGOWkb/N0pHqHajNOkv0eiclOevQXvM6v.",
+      //   is_active: 1,
+      //   reg_id: 1111111,
+      //   password_status: 0,
+      //   reset_code: null,
+      //   image: "https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-1520x800.jpeg",
+      //   role_id: 3,
+      // },
+      // {
+      //   email: "tazeen.athar@szabist-isb.pk",
+      //   password:
+      //       "$2a$08$EwfXXUfdO0fdItc4/kpuGOWkb/N0pHqHajNOkv0eiclOevQXvM6v.",
+      //   is_active: 1,
+      //   reg_id: 2222222,
+      //   password_status: 0,
+      //   reset_code: null,
+      //   image: "https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-1520x800.jpeg",
+      //   role_id: 2,
+      // },
     ], {})
   },
 
